c226bdfb78b70dd37d369a59a87d781009451373
shah <saqlainshahbaltee@gmail.com>
2022-08-28 03:22:37 +0500

    add dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div className="App">
-    <h1>Rinor React Mantine Web App</h1>
-    <h2> Initial Commit</h2>
-  </div>;
+  return (
+    <div>
+      <h1>Rinor Mantine App</h1>
+    </div>
+  );
 }
 
 export default App;
-
